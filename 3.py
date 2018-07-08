4a0a1de4dd8c9a243cba4927a0ec5b107d25b013
puladeepthi <40797103+puladeepthi@users.noreply.github.com>
2018-07-08 13:36:11 +0530

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,5 +1,10 @@
 l = input("Input a letter of the alphabet: ")
+
 if l in ('a', 'e', 'i', 'o', 'u'):
 	print("%s is a vowel." % l)
+elif l == 'y':
+	print("Sometimes letter y stand for vowel, sometimes stand for consonant.")
+elif('b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','z'):
+	print("%s is a consonant." % l) 
 else:
-	print("%s is inavalid." % l) 
+    print("invalid")	
