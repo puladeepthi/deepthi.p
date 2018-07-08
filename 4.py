918ec02b22fd0a4f6e2101dd37586ded23d41fdf
puladeepthi <40797103+puladeepthi@users.noreply.github.com>
2018-07-08 20:14:06 +0530

    update 4.py

--- 4.py
+++ 4.py
@@ -1,5 +1,5 @@
 al=input("enter the alphbet")
 if((al>'a' and al<'z')or(al>'A' and al<'Z')):
-    print(al,"character is alphabet")
+    print(al,"is alphabet")
 else:
-    print(al,"character is not alphabet")
+    print(al,"is not alphabet")
